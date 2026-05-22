ae3e11e26f10bbdd5bcc5736d7c7de78abed06e6
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2026-05-22 20:06:23 -0300

    first commit

---
+++ src/test/java/com/salvadoreduardo/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvadoreduardo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
